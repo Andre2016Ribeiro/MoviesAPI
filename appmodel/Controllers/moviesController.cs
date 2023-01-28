8668d76393fb45a878bb3e4bdfb29dce1723a25a
vitor.andre.ribeiro <vitor.andre.ribeiro@gmail.com>
2023-01-28 23:12:42 +0000

    Adicionar arquivos de projeto.

---
+++ appmodel/Controllers/moviesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using appmodel.Data;
+using appmodel.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public moviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<movie>>> GetMovies()
+        {
+          if (_context.Movies == null)
+          {
+              return NotFound();
+          }
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/movies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<movie>> Getmovie(int id)
+        {
+          if (_context.Movies == null)
+          {
+              return NotFound();
+          }
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // PUT: api/movies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putmovie(int id, movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!movieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/movies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<movie>> Postmovie(movie movie)
+        {
+          if (_context.Movies == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Movies'  is null.");
+          }
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getmovie", new { id = movie.MovieId }, movie);
+        }
+
+        // DELETE: api/movies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletemovie(int id)
+        {
+            if (_context.Movies == null)
+            {
+                return NotFound();
+            }
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool movieExists(int id)
+        {
+            return (_context.Movies?.Any(e => e.MovieId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ appmodel/Program.cs
@@ -0,0 +1,55 @@
+using appmodel.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddCors();
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.UseCors(options =>
+{
+    //Vou abrir completamente esta app web api para todos os pedidos de todos os outros projetos:
+    options.AllowAnyHeader();
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+});
+app.Run();
